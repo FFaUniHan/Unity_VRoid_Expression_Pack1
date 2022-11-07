f59be69d91753732cc2e647584a83fc566b0ec7c
FFaUniHan <handersondavistjia@yahoo.com>
2022-11-07 11:37:01 +0700

    Adding Original Emotes

--- Expression Pack/Assets/Scripts/UIExpressionControl.cs
+++ Expression Pack/Assets/Scripts/UIExpressionControl.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class UIExpressionControl : MonoBehaviour
 {
     [SerializeField] private Animator anim;
+    [SerializeField] private Transform vfxHead;
 
     public void SetExpression(string trigger)
     {
         anim.SetTrigger(trigger);
+    }
+
+    public void InstantiateVFXHead(GameObject prefab)
+    {
+        GameObject temp = Instantiate(prefab, vfxHead);
+        Destroy(temp, 3f);
     }
 }
