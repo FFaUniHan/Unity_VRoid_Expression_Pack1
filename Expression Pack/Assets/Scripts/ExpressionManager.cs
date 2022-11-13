ac2899c04fabb8a319f1aa19c85efe59dab2cb81
FFaUniHan <handersondavistjia@yahoo.com>
2022-11-13 19:01:31 +0700

    Adding More Expressions

--- Expression Pack/Assets/Scripts/ExpressionManager.cs
+++ Expression Pack/Assets/Scripts/ExpressionManager.cs
@@ -9,12 +9,20 @@ public class ExpressionManager: MonoBehaviour
     private Texture extraDefault;
     private Texture eyeDefault;
     private Texture eyeGlowDefault;
+    private Texture faceDefault;
+    private Texture browsDefault;
+    private Texture eyeWhiteDefault;
+    private Texture matCapDefault;
 
     private void Awake()
     {
         extraDefault = face.materials[9].GetTexture("_MainTex");
         eyeDefault = face.materials[5].GetTexture("_MainTex");
         eyeGlowDefault = face.materials[6].GetTexture("_MainTex");
+        faceDefault = face.materials[7].GetTexture("_MainTex");
+        browsDefault = face.materials[4].GetTexture("_MainTex");
+        eyeWhiteDefault = face.materials[1].GetTexture("_MainTex");
+        matCapDefault = face.materials[7].GetTexture("_SphereAdd");
     }
 
     public void SwapTextureExtra(Texture tex)
@@ -43,5 +51,54 @@ public class ExpressionManager: MonoBehaviour
         face.materials[5].SetTexture("_ShadeTexture", eyeDefault);
         face.materials[6].SetTexture("_MainTex", eyeGlowDefault);
         face.materials[6].SetTexture("_ShadeTexture", eyeGlowDefault);
+    }
+
+    public void SwapTextureFace(Texture tex, bool noEmission)
+    {
+        face.materials[7].SetTexture("_MainTex", tex);
+        face.materials[7].SetTexture("_ShadeTexture", tex);
+        face.materials[8].SetTexture("_MainTex", tex);
+        face.materials[8].SetTexture("_ShadeTexture", tex);
+
+        if(noEmission == true)
+        {
+            face.materials[7].SetTexture("_SphereAdd", null);
+            face.materials[8].SetTexture("_SphereAdd", null);
+        }
+    }
+
+    public void SwapTextureFaceReturn()
+    {
+        face.materials[7].SetTexture("_MainTex", faceDefault);
+        face.materials[7].SetTexture("_ShadeTexture", faceDefault);
+        face.materials[8].SetTexture("_MainTex", faceDefault);
+        face.materials[8].SetTexture("_ShadeTexture", faceDefault);
+
+        face.materials[7].SetTexture("_SphereAdd", matCapDefault);
+        face.materials[8].SetTexture("_SphereAdd", matCapDefault);
+    }
+
+    public void SwapTextureBrows(Texture tex)
+    {
+        face.materials[4].SetTexture("_MainTex", tex);
+        face.materials[4].SetTexture("_ShadeTexture", tex);
+    }
+
+    public void SwapTextureBrowsReturn()
+    {
+        face.materials[4].SetTexture("_MainTex", browsDefault);
+        face.materials[4].SetTexture("_ShadeTexture", browsDefault);
+    }
+
+    public void SwapTextureEyeWhite(Texture tex)
+    {
+        face.materials[1].SetTexture("_MainTex", tex);
+        face.materials[1].SetTexture("_ShadeTexture", tex);
+    }
+
+    public void SwapTextureEyeWhiteReturn()
+    {
+        face.materials[1].SetTexture("_MainTex", eyeWhiteDefault);
+        face.materials[1].SetTexture("_ShadeTexture", eyeWhiteDefault);
     }
 }
